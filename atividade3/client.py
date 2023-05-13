28142489bfdb4588530ebe21b21acea654076aa7
Will <50217731+KvWIlY@users.noreply.github.com>
2023-05-12 23:39:52 -0300

    Add files via upload

--- atividade3/client.py
+++ atividade3/client.py
@@ -1,30 +1,33 @@
 import socket
 import threading
+from lib import *
 
 def send_message(sock, message):
-    sock.sendall(message.encode())
+    try:
+        sock.sendall(message.encode())
 
-    response = sock.recv(1024).decode()
-
-    print(f"\n -> Resposta recebida: {response}")
+        response = sock.recv(4096).decode()
+        print(f"Resposta recebida: {response}")
+    except:
+        print("Erro de conexão com o servidor")
+        finalizar_programa()
     
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost',5000)
-    client_socket.connect(server_address)     
+    server_address = ('localhost', 5000)
+    client_socket.connect(server_address)
 
     print('Conectado ao servidor...')
     while True:
-        mensage = input("\n Digite uma mensagem ou QUIT para sair: ")
+        # gera uma mensagem com o tamanho em bytes especificado
+        message = gerar_string(int(sys.argv[1]))
+        print(f"Frase enviada: {message}")
+        send_message(client_socket, message)
 
-        if mensage.lower() == 'quit':
-            break
-
-        send_thread = threading.Thread(target=send_message, args=(client_socket, mensage))
-        send_thread.start()
-
-    client_socket.close()
-
+def init_clients(number_clients):
+    for i in range(number_clients):
+        thread_finalizacao = threading.Thread(target=start_client)
+        thread_finalizacao.start()
 
 if __name__ == "__main__":
-    start_client()
+    init_clients(1)
--- atividade3/server.py
+++ atividade3/server.py
@@ -1,25 +1,26 @@
 import socket
 import threading
+from lib import *
+
+def echo_reply_reverse(client_socket, message: str):
+    # Inverte a mensagem recebida
+    message = message.decode()[::-1].encode()
+
+    # Envia a mensagem invertida de volta para o cliente
+    client_socket.send(message)
 
 def handle_client(client_socket):
     while True:
-        try:
-            # Recebe a mensagem do cliente
-            data = client_socket.recv(1024)
+        # Recebe a mensagem do cliente
+        message = client_socket.recv(4096)
 
-            if not data:
-                print(f"Cliente {client_socket.getpeername()} se desconectou.")
-                break
+        # inicia uma nova thread para processar a requisicao
+        nova_requisicao = threading.Thread(target=echo_reply_reverse, args=(client_socket, message))
+        nova_requisicao.start()
 
-             # Inverte a mensagem recebida
-            message = data.decode()[::-1].encode()
-
-            # Envia a mensagem invertida de volta para o cliente
-            client_socket.send(message)
-        except:
-            print(f"Erro na conexão com o cliente {client_socket.getpeername()}")
+        if not message:
+            client_socket.close()
             break
-    client_socket.close()
 
 def start_server():
     # Cria um socket TCP/IP
@@ -38,8 +39,18 @@ def start_server():
         # Aceita uma conexão e cria uma nova thread para atendê-la
         client_socket, client_address = server_socket.accept()
         print(f'Nova conexão de {client_address}')
+
+        # cria uma thread para cada tratar de cada novo cliente
         client_thread = threading.Thread(target=handle_client, args=(client_socket,))
         client_thread.start()
 
+
 if __name__ == '__main__':
+    # tamanho das mensagens recebidas em bytes
+    tamanho_mensagem = int(sys.argv[1])
+
+    # gera logs de uso em tempo real
+    thread_log_generator = threading.Thread(target=gera_logs, args=(tamanho_mensagem,))
+    thread_log_generator.start()
+
     start_server()
